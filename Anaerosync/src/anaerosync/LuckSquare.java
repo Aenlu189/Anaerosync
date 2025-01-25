76760508966a70af8087acde491e91196c688215
40408771 <aelserafy01@qub.ac.uk>
2025-01-25 17:39:13 +0000

    Upload New File

---
+++ Anaerosync/src/anaerosync/LuckSquare.java
@@ -0,0 +1,17 @@
+public class LuckSquare {
+    private int id;
+    private SquareType squareType;
+
+    public LuckSquare(int id, SquareType squareType) {
+        this.id = id;
+        this.squareType = squareType;
+    }
+
+    public void landOnSquare(Player player) {
+
+    }
+
+    private void drawLuckCard() {
+
+    }
+}
