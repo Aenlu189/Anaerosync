88454f3c2d1bcc8184d9e569898d5877ee07c8a5
40408771 <aelserafy01@qub.ac.uk>
2025-01-25 17:40:22 +0000

    Upload New File

---
+++ Anaerosync/src/anaerosync/StartSquare.java
@@ -0,0 +1,13 @@
+public class StartSquare {
+    private int id;
+    private SquareType squareType;
+
+    public StartSquare(int id, SquareType squareType) {
+        this.id = id;
+        this.squareType = squareType;
+    }
+
+    public void landOnSquare(Player player) {
+
+    }
+}
