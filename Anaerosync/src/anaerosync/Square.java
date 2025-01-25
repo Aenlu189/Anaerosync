bbb2c1ebd817bcd7c0c8a17c70ceea668b358a55
40408771 <aelserafy01@qub.ac.uk>
2025-01-25 17:39:46 +0000

    Upload New File

---
+++ Anaerosync/src/anaerosync/Square.java
@@ -0,0 +1,13 @@
+public abstract class Square {
+    private int id;
+    private SquareType squareType;
+
+    public Square(int id, SquareType squareType) {
+        this.id = id;
+        this.squareType = squareType;
+    }
+
+    public void activateSquare(Player player) {
+
+    }
+}
