75d8a81246c4b0a2e462f84e00218da4118cf143
Sebastian <svaldes04@gmail.com>
2025-03-18 17:53:58 +0000

    PositionManager tests

--- Anaerosync/src/main/java/aureo/anaerosync/boardGameController.java
+++ Anaerosync/src/main/java/aureo/anaerosync/boardGameController.java
@@ -116,6 +116,8 @@ public class boardGameController {
         new Player(4, "Player 4", STARTING_TIME, STARTING_MONEY, SHARED_TRUST)
     };
 
+    private PositionManager positionManager;
+
     // ArrayList containing all tasks in the game
     private final ArrayList<Task> tasks = new ArrayList<Task>();
     private final ArrayList<Objective> objectives = new ArrayList<Objective>();
@@ -185,7 +187,7 @@ public class boardGameController {
         initializeEventSquares();
 
         // Initialize the board with tasks and luck cards
-        PositionManager.initializeSquares(tasks);
+        positionManager.initializeSquares(tasks);
 
         // Hide all player pieces initially
         hideAllPlayers();
@@ -784,7 +786,7 @@ public class boardGameController {
 
     // Check if the player is on Task Square or Other squares like corners and luck card
     private void checkPosition(int position) {
-        Square square = PositionManager.getSquareAtPosition(position);
+        Square square = positionManager.getSquareAtPosition(position);
         System.out.println("Player landed on " + square.getType() + " at position " + position);
 
         if (square instanceof TaskSquare) {
