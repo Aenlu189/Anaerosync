bea8160e847c33f1a86edf5dc513134a73f8c30b
40392287 <icarbonellsanchez01@qub.ac.uk>
2025-03-19 16:42:48 +0000

    Fixed Contribute fee, die, and task

--- Anaerosync/src/main/java/aureo/anaerosync/boardGameController.java
+++ Anaerosync/src/main/java/aureo/anaerosync/boardGameController.java
@@ -111,9 +111,9 @@ public class boardGameController {
 
     private final Random random = new Random();
 
-    private static final int STARTING_MONEY = 100;
-    private static final int STARTING_TIME = 100;
-    private static int SHARED_TRUST = 100;
+    private static final int STARTING_MONEY = 1000;
+    private static final int STARTING_TIME = 1000;
+    private static int SHARED_TRUST = 0;
     private static final int TOTAL_TASKS = 20;
 
     /**
@@ -424,7 +424,7 @@ public class boardGameController {
     public void rollDice() {
         if (!hasMoved) {
             int roll = random.nextInt(6) + 1;
-            diceResult.setText("Dice: " + roll);
+            diceResult.setText("Die: " + roll);
 
             // Update the dice image based on the roll result
             String diceImagePath = String.format("/images/Die%d.png", roll);
@@ -453,7 +453,7 @@ public class boardGameController {
         currentPlayer = (currentPlayer + 1) % numPlayers;
 
         // Show whose turn it is
-        showErrorDialog.setText(String.format("%s's turn. Roll the dice", players[currentPlayer].getName()));
+        showErrorDialog.setText(String.format("%s's turn. Roll the die", players[currentPlayer].getName()));
         showErrorDialog.setStyle("-fx-text-fill: white;");
 
         // Check if there's an active DDOS Attack
@@ -536,26 +536,26 @@ public class boardGameController {
 
     // Initialization using Ignacio's tasks
     private void initializeTasks() {
-        tasks.add(new Task(1, "Research on Anaerobic Digestion", 50, 0, 50, 10, 25, "RESEARCH", "/images/1.png", 10, 10));
-        tasks.add(new Task(2, "Research on Makers Valley", 60, 0, 65, 12, 25, "RESEARCH", "/images/2.png", 12, 13));
-        tasks.add(new Task(3, "Design Use Cases", 80, 0, 80, 16, 50, "SKETCHING", "/images/3.png", 16, 16));
-        tasks.add(new Task(4, "Design UML Diagram", 85, 0, 100, 17, 50, "SKETCHING", "/images/4.png", 17, 20));
-        tasks.add(new Task(5, "Develop Visual Design on Figma", 105, 0, 120, 21, 75, "FRONT-END", "/images/5.png", 21, 24));
-        tasks.add(new Task(6, "Start Developing in JavaScript", 120, 0, 135, 24, 75, "FRONT-END", "/images/6.png", 24, 27));
-        tasks.add(new Task(7, "Develop Classes", 135, 0, 155, 27, 100, "BACK-END", "/images/7.png", 27, 31));
-        tasks.add(new Task(8, "Implement Functions and Methods", 150, 0, 180, 30, 100, "BACK-END", "/images/8.png", 30, 36));
-        tasks.add(new Task(9, "Design Test Cases", 190, 0, 185, 38, 125, "TESTING", "/images/9.png", 38, 37));
-        tasks.add(new Task(10, "Implement Testing Acceptance Plan", 230, 0, 210, 46, 125, "TESTING", "/images/10.png", 46, 42));
-        tasks.add(new Task(11, "Implement Cookies System", 245, 300, 240, 47, 150, "CYBER-SECURITY", "/images/11.png", 49, 48));
-        tasks.add(new Task(12, "Buy Domain SSL Certificate", 235, 250, 235, 49, 150, "CYBER-SECURITY", "/images/12.png", 47, 47));
-        tasks.add(new Task(13, "Sell Subscriptions to Hardware Stores", 270, 400, 265, 54, 175, "DEPLOYMENT", "/images/13.png", 54, 53));
-        tasks.add(new Task(14, "Add Videos and Images", 290, 600, 285, 58, 175, "DEPLOYMENT", "/images/14.png", 58, 57));
-        tasks.add(new Task(15, "Partner with Lock", 300, 800, 300, 60, 200, "PARTNERSHIPS", "/images/15.png", 60, 60));
-        tasks.add(new Task(16, "Partner with Hardware Stores", 315, 1000, 320, 63, 200, "PARTNERSHIPS", "/images/16.png", 63, 64));
-        tasks.add(new Task(17, "Market Research", 350, 1500, 340, 70, 225, "MARKETING", "/images/17.png", 70, 68));
-        tasks.add(new Task(18, "Content Development", 375, 2000, 355, 75, 225, "MARKETING", "/images/18.png", 75, 71));
-        tasks.add(new Task(19, "Maintaining AnaeroSync", 395, 3000, 375, 79, 250, "SUSTAINABILITY", "/images/19.png", 79, 75));
-        tasks.add(new Task(20, "Updating AnaeroSync", 400, 4000, 400, 80, 250, "SUSTAINABILITY", "/images/20.png", 80, 80));
+        tasks.add(new Task(1, "Research on Anaerobic Digestion", 50, 0, 50, 10, 25, "RESEARCH", "/images/1.png", 10, 10, 10));
+        tasks.add(new Task(2, "Research on Makers Valley", 60, 0, 65, 12, 25, "RESEARCH", "/images/2.png", 12, 13, 10));
+        tasks.add(new Task(3, "Design Use Cases", 80, 0, 80, 16, 50, "SKETCHING", "/images/3.png", 16, 16, 20));
+        tasks.add(new Task(4, "Design UML Diagram", 85, 0, 100, 17, 50, "SKETCHING", "/images/4.png", 17, 20, 20));
+        tasks.add(new Task(5, "Develop Visual Design on Figma", 105, 0, 120, 21, 75, "FRONT-END", "/images/5.png", 21, 24, 30));
+        tasks.add(new Task(6, "Start Developing in JavaScript", 120, 0, 135, 24, 75, "FRONT-END", "/images/6.png", 24, 27, 30));
+        tasks.add(new Task(7, "Develop Classes", 135, 0, 155, 27, 100, "BACK-END", "/images/7.png", 27, 31, 40));
+        tasks.add(new Task(8, "Implement Functions and Methods", 150, 0, 180, 30, 100, "BACK-END", "/images/8.png", 30, 36, 40));
+        tasks.add(new Task(9, "Design Test Cases", 190, 0, 185, 38, 125, "TESTING", "/images/9.png", 38, 37,50));
+        tasks.add(new Task(10, "Implement Testing Acceptance Plan", 230, 0, 210, 46, 125, "TESTING", "/images/10.png", 46, 42, 50));
+        tasks.add(new Task(11, "Implement Cookies System", 245, 300, 240, 47, 150, "CYBER-SECURITY", "/images/11.png", 49, 48, 60));
+        tasks.add(new Task(12, "Buy Domain SSL Certificate", 235, 250, 235, 49, 150, "CYBER-SECURITY", "/images/12.png", 47, 47, 60));
+        tasks.add(new Task(13, "Sell Subscriptions to Hardware Stores", 270, 400, 265, 54, 175, "DEPLOYMENT", "/images/13.png", 54, 53, 70));
+        tasks.add(new Task(14, "Add Videos and Images", 290, 600, 285, 58, 175, "DEPLOYMENT", "/images/14.png", 58, 57, 70));
+        tasks.add(new Task(15, "Partner with Lock", 300, 800, 300, 60, 200, "PARTNERSHIPS", "/images/15.png", 60, 60, 80));
+        tasks.add(new Task(16, "Partner with Hardware Stores", 315, 1000, 320, 63, 200, "PARTNERSHIPS", "/images/16.png", 63, 64, 80));
+        tasks.add(new Task(17, "Market Research", 350, 1500, 340, 70, 225, "MARKETING", "/images/17.png", 70, 68, 90));
+        tasks.add(new Task(18, "Content Development", 375, 2000, 355, 75, 225, "MARKETING", "/images/18.png", 75, 71, 90));
+        tasks.add(new Task(19, "Maintaining AnaeroSync", 395, 3000, 375, 79, 250, "SUSTAINABILITY", "/images/19.png", 79, 75, 100));
+        tasks.add(new Task(20, "Updating AnaeroSync", 400, 4000, 400, 80, 250, "SUSTAINABILITY", "/images/20.png", 80, 80, 100));
 
         tasks.getFirst().setClaimMessage("Research is the foundation of AnaeroSync. Investigate the economic, environmental, and social factors of anaerobic digestion. Invest 50 units of Money to claim this Task. You are not required any Community Trust units for this task, however, receive 25 Community Trust units as a reward for claiming this task.");
         tasks.get(1).setClaimMessage("Understanding the local environment is key for AnaeroSync's success. Investigate the waste management situation in Makers Valley and potential opportunities. Invest 60 units of Money to claim this Task. You are not required any Community Trust units for this task, however, receive 25 Community Trust units as a reward for claiming this task.");
@@ -888,6 +888,7 @@ public class boardGameController {
         Player currentPlayerObj = players[currentPlayer];
         int feeMoney = task.getFeeMoney();
         int feeTime = task.getFeeTime();
+        int feeTrust = task.getFeeTrust();
 
         // Set the task card image
         try {
@@ -922,9 +923,11 @@ public class boardGameController {
                 // Add resources to task owner
                 owner.setMoneyResource(owner.getMoneyResource() + feeMoney);
                 owner.setTimeResource(owner.getTimeResource() + feeTime);
+                SHARED_TRUST += feeTrust;
 
-                showErrorDialog.setText(String.format("Paid fee to %s: %d money and %d time",
-                        owner.getName(), feeMoney, feeTime));
+                showErrorDialog.setText(String.format("Paid fee to %s: %d money and %d time. Receive %d community trust as a reward for helping.",
+                        owner.getName(), feeMoney, feeTime, feeTrust));
+
 
                 // Check for lose condition after deducting resources
                 if (currentPlayerObj.getMoneyResource() <= 0 || currentPlayerObj.getTimeResource() <= 0) {
@@ -1382,8 +1385,7 @@ public class boardGameController {
 
             // Deduct resources
             player.setMoneyResource(player.getMoneyResource() - task.getTaskMoney());
-            player.setTimeResource(player.getTimeResource() - task.getTaskTime());
-            SHARED_TRUST -= task.getTaskTrustNeeded();
+            SHARED_TRUST += task.getTaskBonus();
 
             // Add task to player's owned tasks
             player.addTask(task);
@@ -1896,6 +1898,7 @@ public class boardGameController {
                 // Show success message
                 showErrorDialog.setText("Task completed successfully!");
 
+
                 // Check if this completes an objective
                 rewardObjective(currentPlayerObj, selectedTaskToComplete);
 
@@ -1971,6 +1974,8 @@ public class boardGameController {
                             objective.getObjectiveMoney() + ", " +
                             objective.getObjectiveTime() + " time, and " +
                             objective.getObjectiveTrust() + " trust.");
+
+                    messageBox.setText(objective.getCompleteObjectiveMessage());
 
                     // No need to check other objectives since a task can only be part of one objective
                     break;
@@ -2168,13 +2173,13 @@ public class boardGameController {
                 break;
             case "Donate Money":
                 description.append("You gain 200 Trust for the community. ");
-                description.append("Roll the dice again. If you roll 5, move 5 spaces and lose (roll × 100) money.");
+                description.append("Roll the die again. If you roll 5, move 5 spaces and lose (roll × 100) money.");
                 break;
             case "Home":
                 description.append("Welcome home! You receive 100 money and 100 time.");
                 break;
             case "Receive Funds":
-                description.append("Roll the dice again. You will receive 50 × (dice roll) money.");
+                description.append("Roll the die again. You will receive 50 × (dice roll) money.");
                 break;
         }
 
